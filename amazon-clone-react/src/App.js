fea676a7def0ffee6e7f7618550da94086a5e662
kannan-santharam <kannan.santharam@freshworks.com>
2020-08-02 11:13:04 +0530

    amazon clone setup

---
+++ amazon-clone-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Amazon Clone</h1>
+    </div>
+  );
+}
+
+export default App;
