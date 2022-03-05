45ab573911c755554b1becfbf087887bfbc2d20a
kannan-santharam <kannan.santharam@freshworks.com>
2022-03-05 09:59:12 +0530

    added todolist component

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import TodoList from './TodoList'
+function addTodo() {
+  let inputValue = document.getElementById("todoValue").value;
+  console.log(inputValue)
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>My todo app</h2>
+        <input type="text" id="todoValue" placeholder="Enter todo" />
+        <button type="button" onClick={addTodo} className="btn btn-primary">Add Todo</button>
       </header>
+      <section>
+        <TodoList />
+      </section>
     </div>
   );
 }
---
+++ todo-react/src/TodoList.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function TodoList() {
+    return (
+        <div>a</div>
+    );
+}
+
+export default TodoList;
