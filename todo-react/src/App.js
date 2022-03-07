018112fc2f94c34b888a608bd966f77f2f06f8e8
kannan-santharam <kannan.santharam@freshworks.com>
2022-03-07 14:02:36 +0530

    remove todo feature added

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -3,20 +3,24 @@ import './App.css';
 import TodoList from './TodoList'
 function App() {
   const [myTodos, setmyTodos] = useState([]);
-  const addTodo = () => {
+  const addTodo = (stateTodos) => {
     let inputValue = document.getElementById("todoValue").value;
-    console.log(inputValue);
+    document.getElementById("todoValue").value = '';
+    if (stateTodos) {
+      setmyTodos(() => [...stateTodos]);
+      return
+    }
     setmyTodos((myTodos) => [...myTodos, inputValue])
   }
   return (
     <div className="App">
       <header className="App-header">
         <input type="text" id="todoValue" placeholder="Enter todo" />
-        <button type="button" onClick={addTodo} className="btn btn-primary">Add Todo</button>
+        <button type="button" onClick={e => addTodo("")} className="primary" >Add Todo</button>
       </header>
       <section>
         <ul>
-          <TodoList todo={myTodos} />
+          <TodoList todo={myTodos} updateState={addTodo} />
         </ul>
       </section>
     </div>
--- todo-react/src/TodoList.js
+++ todo-react/src/TodoList.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function TodoList(myTodos) {
+    const removeTodo = (e, myTodos) => {
+        let value = e.target.textContent;
+        let stateTodos = myTodos.todo;
+        let stateValueIndex = stateTodos.findIndex(a => a === value)
+        if (stateValueIndex !== -1) {
+            stateTodos.splice(stateValueIndex, 1);
+        };
+        console.log(stateTodos);
+        myTodos.updateState(stateTodos)
+        ///e.target.parentElement.remove();
+
+    }
     return (
         myTodos.todo.map((myTodo, index) => {
             return (
-                <li key={myTodo + "_" + index}>
+                <li key={myTodo + "_" + index} onClick={e => removeTodo(e, myTodos)}>
                     <p>{myTodo}</p>
                 </li>
             )
