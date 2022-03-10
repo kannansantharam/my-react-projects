603cd8a3981ca135b830d5003adca142dfe8be8f
kannan-santharam <kannan.santharam@freshworks.com>
2022-03-10 21:38:17 +0530

    ecommerce project initial setup

---
+++ ecommerce-store-react/README.md
@@ -0,0 +1,2 @@
+Builing an e-commerce website with sample products
+Pulling all the product details from https://fakestoreapi.com/
---
+++ ecommerce-store-react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Fake Store
+    </div>
+  );
+}
+
+export default App;
