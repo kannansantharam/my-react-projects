93bb603f1f55023ec4590de2190f33b9347e31df
kannan-santharam <kannan.santharam@freshworks.com>
2022-03-11 19:49:41 +0530

    black jack card initial setup + draw cards

---
+++ black-jack-card-game-react/src/App.js
@@ -0,0 +1,12 @@
+import BlackJack from './Blackjack';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BlackJack></BlackJack>
+    </div>
+  );
+}
+
+export default App;
