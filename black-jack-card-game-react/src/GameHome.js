dec6a5d5b060aa7dacad999f6b3de1dab2e4e9c0
kannan-santharam <kannan.santharam@freshworks.com>
2022-03-12 09:45:02 +0530

    validation added, if the total score is more than 21 stop the game

--- black-jack-card-game-react/src/GameHome.js
+++ black-jack-card-game-react/src/GameHome.js
@@ -35,48 +35,77 @@ function GameHome() {
             setCurrentPlayerScore(totalValue) : setCurrentDealerScore(totalValue);
         //currentPlayerScore = currentPlayerScore ? currentPlayerScore : 0;
         totalValue = parseInt(totalPlayerScore) + parseInt(totalValue);
-        console.log("total score ", totalValue)
-        if (totalValue > 21) {
-            totalValue = "You Lost, Total points " + totalValue;
-        }
         type === "Player" ?
             setTotalPlayerScore(totalValue) : setTotalDealerScore(totalValue);
+        if (totalValue > 21) {
+            // totalValue = "You Lost, Total points " + totalValue;
+            document.querySelector(".game-over-section").classList.remove("hide");
+            document.querySelector(".main-button-section").classList.add("hide")
+            return
+        }
+
+        return totalValue;
     }
-    const drawCards = async (e, type) => {
+    const drawCards = async (type) => {
+
         setCurretPlayer(type);
         await axios.get(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=2`)
             .then((data) => {
-                setDrawnCard(data.data.cards);
-                calculateTotalValue(data.data.cards, type);
+                //setDrawnCard(data.data.cards);
+                let totalValues = calculateTotalValue(data.data.cards, type);
+                if (!totalValues) {
+                    return
+                }
                 if (type === "Player") {
+                    document.querySelector(".drawCardsBtn").innerHTML = "Dealer drawing the cards, hold on"
                     setCurretPlayer("Dealer")
                     setTimeout(() => {
-                        drawCards(e, "Dealer")
+                        drawCards("Dealer");
                     }, 2000)
                 } else {
+                    document.querySelector(".drawCardsBtn").innerHTML = "Your turn! Draw Cards"
                     setCurretPlayer("Player")
                 }
 
             })
     }
+    function restartGame() {
+        window.location.reload()
+    }
+    function onPlayerChoice(type) {
+        document.querySelectorAll(".initial-button").forEach(function (value) {
+            value.style.display = "none";
+        });
+        document.querySelector(".drawing-cards").classList.remove("hide")
+        if (type === "Player") {
+            document.querySelector(".drawCardsBtn").innerHTML = "Your turn! Draw cards";
+        } else {
+            document.querySelector(".drawCardsBtn").innerHTML = "Dealer draws the cards, hold on";
+            drawCards("Dealer")
+        }
+    }
     return (
         <div>
             <h1>Welcome to Cards Game</h1>
+
             <div className='main-section'>
                 <p>There are two players, Player and Dealer</p>
-                <h5>Player plays first, click on the button to draw cards </h5>
+            </div>
+            <div className='game-over-section hide'>
+                <h3>Oops! {curretPlayer} lost. Click on the below button to restart the game</h3>
+                <Button variant="contained" className="primary " color="primary" onClick={restartGame}> Restart the game</Button>
             </div>
             <div className="main-button-section">
-                As a Player you have an choice, click on the <i>Hit</i> button to play or
-                <i> Stand</i> button to give chance to the Dealer
-                <br />
-                <Button variant="contained" className="primary" color="primary" onClick={e => drawCards(e, "Player")}>Hit (Play)</Button>
-                <Button variant="contained" className="primary" color="primary" onClick={e => drawCards(e, "Dealer")}> Stand (Give chance to Dealer)</Button>
+                <p className="initial-button"><b>Player plays first,</b> and as a Player you have an choice, click on the <i>Hit</i> button to play or
+                    <i> Stand</i> button to give chance to the Dealer</p>
 
-                <div className='drawing-cards'>
+                <Button variant="contained" className="primary initial-button" color="primary" onClick={() => onPlayerChoice("Player")}>Hit (Play)</Button>
+                <Button variant="contained" className="primary initial-button" color="primary" onClick={() => onPlayerChoice("Dealer")}> Stand (Give chance to Dealer)</Button>
+
+                <div className='drawing-cards hide'>
                     <p className="dealer-playing hide">{curretPlayer} drawing the cards</p>
                     <p className="player-playing hide">It's your turn, draw the cards</p>
-                    <Button variant="contained" className="primary hide" color="primary" onClick={e => drawCards(e, "Player")}> Draw Cards</Button>
+                    <Button variant="contained" className="primary hide drawCardsBtn" color="primary" onClick={() => drawCards("Player")}> Your turn! Draw cards</Button>
                     <p>Your current score is {currentPlayerScore}</p>
                     <p>Dealer's current score is {currentDealerScore}</p>
                 </div>
