3891ffbd25f41b145e0af235d041b984ffa51303
kannan-santharam <kannan.santharam@freshworks.com>
2022-04-10 15:39:39 +0530

    allow user to select only one reaction at a time + active reaction state

--- likes-reaction-react/src/Reaction/Reactions.js
+++ likes-reaction-react/src/Reaction/Reactions.js
@@ -2,19 +2,55 @@ import { useReducer } from "react";
 import { HeartIcon, Like, Clap } from './Icons'
 import ReactionIcons from './ReactionIcons'
 function reducer(state, action) {
-    console.log(state)
+    let newState = {};
     switch (action.type) {
         case 'LikeCount':
             let like = state.toggleLike ? state.LikeCount + 1 : state.LikeCount - 1;
-            return { ...state, LikeCount: like, toggleLike: !state.toggleLike };
+            newState = {
+                LikeCount: like,
+                toggleLike: !state.toggleLike
+            };
+            if (!state.toggleHeart) {
+                newState["HeartCount"] = state.HeartCount - 1;
+                newState["toggleHeart"] = !state.toggleHeart
+            }
+            if (!state.toggleClap) {
+                newState["ClapCount"] = state.ClapCount - 1;
+                newState["toggleClap"] = !state.toggleClap
+            }
+            return { ...state, ...newState };
 
         case 'HeartCount':
             let heart = state.toggleHeart ? state.HeartCount + 1 : state.HeartCount - 1;
-            return { ...state, HeartCount: heart, toggleHeart: !state.toggleHeart };
+            newState = {
+                HeartCount: heart,
+                toggleHeart: !state.toggleHeart
+            };
+            if (!state.toggleLike) {
+                newState["LikeCount"] = state.LikeCount - 1;
+                newState["toggleLike"] = !state.toggleLike
+            }
+            if (!state.toggleClap) {
+                newState["ClapCount"] = state.ClapCount - 1;
+                newState["toggleClap"] = !state.toggleClap
+            }
+            return { ...state, ...newState };
 
         case 'ClapCount':
             let clap = state.toggleClap ? state.ClapCount + 1 : state.ClapCount - 1;
-            return { ...state, ClapCount: clap, toggleClap: !state.toggleClap };
+            newState = {
+                ClapCount: clap,
+                toggleClap: !state.toggleClap
+            };
+            if (!state.toggleLike) {
+                newState["LikeCount"] = state.LikeCount - 1;
+                newState["toggleLike"] = !state.toggleLike
+            }
+            if (!state.toggleHeart) {
+                newState["HeartCount"] = state.HeartCount - 1;
+                newState["toggleHeart"] = !state.toggleHeart
+            }
+            return { ...state, ...newState };
 
         default:
             return state
@@ -39,15 +75,15 @@ function Reactions() {
 
             <div className="reaction-counts">
                 {state.LikeCount ?
-                    <div className="like-count icon-count" onClick={() => updateCounts("LikeCount")}>
+                    <div className={"like-count icon-count " + (!state.toggleLike ? 'active' : '')} onClick={() => updateCounts("LikeCount")}>
                         {Like}  {state.LikeCount}
                     </div> : ''}
                 {state.HeartCount ?
-                    <div className="heart-count icon-count" onClick={() => updateCounts("HeartCount")}>
+                    <div className={"heart-count icon-count " + (!state.toggleHeart ? 'active' : '')} onClick={() => updateCounts("HeartCount")}>
                         {HeartIcon}  {state.HeartCount}
                     </div> : ''}
                 {state.ClapCount ?
-                    <div className="clap-count icon-count" onClick={() => updateCounts("ClapCount")}>
+                    <div className={"clap-count icon-count " + (!state.toggleClap ? 'active' : '')} onClick={() => updateCounts("ClapCount")}>
                         {Clap} {state.ClapCount}
                     </div> : ''}
             </div>
