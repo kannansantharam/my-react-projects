1990f3a01245bbbba1cbcc1ea20d212104bb626c
kannan-santharam <kannan.santharam@freshworks.com>
2022-04-10 17:25:00 +0530

    image url updated + summary component created

--- likes-reaction-react/src/Posts/Posts.js
+++ likes-reaction-react/src/Posts/Posts.js
@@ -15,12 +15,12 @@ function Posts() {
     return (
         <div className="posts-section">
             <div className="image-post">
-                <img src="https://raw.githubusercontent.com/kannansantharam/my-react-projects/development/likes-reaction-react/src/Images/isro.jpeg" alt="isro" />
+                <img src="https://raw.githubusercontent.com/kannansantharam/my-react-projects/development/likes-reaction-react/src/Posts/isro.jpeg" alt="isro" />
                 <Reactions like={578} heart={274} clap={95} />
                 <Comments username="Kannan Santharam" content="Hi there, How are you? Look" />
             </div>
             <div className="image-post">
-                <img src="https://raw.githubusercontent.com/kannansantharam/my-react-projects/development/likes-reaction-react/src/Images/rocket.jpeg" alt="rocket" />
+                <img src="https://raw.githubusercontent.com/kannansantharam/my-react-projects/development/likes-reaction-react/src/Posts/rocket.jpeg" alt="rocket" />
                 <Reactions like={19} heart={4} clap={45} />
             </div>
 
---
+++ likes-reaction-react/src/Reaction/Summary.js
@@ -0,0 +1,7 @@
+function Summary() {
+    return (
+        <div>
+
+        </div>
+    )
+}
